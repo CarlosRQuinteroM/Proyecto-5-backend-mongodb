297e746d2d3ae1c1f5aff053f8aa2c98b1262235
juanfegallego <juanfegallegodj@gmail.com>
2021-06-02 11:17:28 +0200

    rest routes modify

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -10,3 +10,36 @@ router.get('/', async (req, res) => {
         });
     }
 });
+
+router.post('/', async (req,res) => {
+    try {
+        const user = req.body;
+        res.json(await userController.createUser(user))
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+})
+//, authenticate
+router.put("/", async(req, res) => {
+    try {
+        const body = req.body;
+        res.json( await usersControllers.modifyUser(body));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+//, authenticate
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        res.json(await usersControllers.deleteUser(id));
+    }catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+});
